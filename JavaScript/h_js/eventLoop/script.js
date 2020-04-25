968c02bbb52d09d4d51f19eb5f96c45d504a68f9
Denis <denfedweb@mail.ru>
2020-04-25 19:42:23 +0300

    denfedweb

--- JavaScript/h_js/eventLoop/script.js
+++ JavaScript/h_js/eventLoop/script.js
@@ -1,5 +1,22 @@
-setTimeout(()=>{
-    console.log("one");
-}, 0);
+// setTimeout(()=>{
+//     console.log("one");
+// }, 0);
 
-console.log("two");
+// console.log("two");
+
+
+function addDigits(num) {
+    const x = [...num.toString()];
+    
+    const newNum = x.reduce((total, digit) =>{
+        return total + Number(digit);
+    }, 0)
+    if(x.length === 1){
+        return newNum;    
+    }  
+    return addDigits(newNum); 
+}
+
+
+const finalValue = addDigits(1259);
+console.log(finalValue);
