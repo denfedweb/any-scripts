eb25b3ef5a76b99a7957de404536782bf43507ab
denfedweb <denfedweb@mail.ru>
2020-05-14 08:59:56 +0300

    denfedweb

---
+++ JavaScript/h_js/MutationObserver/MutationObserver.js
@@ -0,0 +1,12 @@
+let observer = new MutationObserver(mutationRecords => {
+    console.log(mutationRecords); // console.log(изменения)
+  });
+  
+  // наблюдать за всем, кроме атрибутов
+  observer.observe(elem, {
+    childList: true, // наблюдать за непосредственными детьми
+    subtree: true, // и более глубокими потомками
+    characterDataOldValue: true // передавать старое значение в колбэк
+  });
+
+
