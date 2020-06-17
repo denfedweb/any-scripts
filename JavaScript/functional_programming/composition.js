e6dc512bddf84833b6976157e5c8193d16a26c42
denfedweb <denfedweb@mail.ru>
2020-06-17 18:02:48 +0300

    11

--- JavaScript/functional_programming/composition.js
+++ JavaScript/functional_programming/composition.js
@@ -9,3 +9,21 @@ const compose = (...fns) => x => fns.reduceRight((acc, fn) => fn(acc), x);
 
 const number = compose(Math.round, parseFloat);
 number('43.45'); // 43
+
+// //////////////////////
+
+const composeAsyncFunctions = (...fns) => arg =>
+  fns.reduce((p, fn) => p.then(fn), Promise.resolve(arg));
+
+// Пример:
+
+const sum = composeAsyncFunctions(
+  x => x + 1,
+  x => new Promise(resolve => setTimeout(() => resolve(x + 2), 1000)),
+  x => x + 3,
+  async x => (await x) + 4
+);
+
+(async () => {
+  console.log(await sum(2)); // 12 (Через одну секунду)
+})();
